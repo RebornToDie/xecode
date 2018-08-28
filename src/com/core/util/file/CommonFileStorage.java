01c58498239dddd3e69725960ac039497faf47b1
xh <1481719687@qq.com>
2018-08-28 19:01:32 +0800

    文件存储

--- src/com/core/util/file/CommonFileStorage.java
+++ src/com/core/util/file/CommonFileStorage.java
@@ -1,10 +1,21 @@
 package com.core.util.file;
 
-import java.io.*;
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.*;
+import java.net.URLDecoder;
+
+/**
+ *文件存储
+ * @author reborntodie
+ */
 public class CommonFileStorage implements IFileStorage {
+    private static Logger log = LogManager.getLogger(CommonFileStorage.class);
+
     @Override
-    public long saveFile(String aboslutePath, InputStream input) {
+    public long saveFile(String aboslutePath, InputStream input) throws IOException {
         long size = -1;
         File target = new File(aboslutePath);
         if(!target.getParentFile().exists()){
@@ -14,63 +25,136 @@ public class CommonFileStorage implements IFileStorage {
         OutputStream newOut = null;
         try{
             newOut = new BufferedOutputStream(new FileOutputStream(target));
-            size = I
+            size = IOUtils.copyLarge(input , newOut);
         }catch (Exception e){
-
+            log.error("move file error : " + e);
+            throw new IOException(e);
         }finally{
-
+            IOUtils.closeQuietly(input);
+            IOUtils.closeQuietly(newOut);
         }
-
-
+        return size;
     }
 
     @Override
-    public boolean createNewFile(String aboslutePath) {
-        return false;
+    public boolean createNewFile(String aboslutePath) throws IOException {
+        File target = new File(aboslutePath);
+        if(!target.getParentFile().exists()){
+            target.getParentFile().mkdirs();
+        }
+        //文件存在返回false
+        if(target.exists()){
+            return false;
+        }
+        return target.createNewFile();
+    }
+
+
+    @Override
+    public boolean deleteFile(String absolutePath) throws IOException{
+        File target = new File(absolutePath);
+        if(!target.exists()){
+            log.warn("the path can not be found : " + absolutePath);
+            return false;
+        }
+        if(!target.isFile()){
+            log.warn("the path is not a file : " + absolutePath);
+            return false;
+        }
+        return target.delete();
     }
 
     @Override
-    public boolean deleteFile(String absolutePath) {
-        return false;
+    public boolean exists(String absolutePath) throws IOException{
+        File target =  new File(absolutePath);
+        return target.exists();
     }
 
     @Override
-    public boolean exists(String absolutePath) {
-        return false;
+    public InputStream getInputStream(String absolutePath) throws IOException{
+        try {
+            absolutePath = URLDecoder.decode(absolutePath , "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            log.warn("url encode error : " + e);
+        }
+        //get from local file system
+        File target = new File(absolutePath);
+        return new BufferedInputStream(new FileInputStream(target));
     }
 
     @Override
-    public InputStream getInputStream(String absolutePath) {
-        return null;
+    public OutputStream getOutputStream(String absolutePath) throws IOException{
+        try {
+            absolutePath = URLDecoder.decode(absolutePath,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            log.warn("url encode error : " + e);
+        }
+        //get from local file system
+        File target = new File(absolutePath);
+        return new BufferedOutputStream(new FileOutputStream(target));
     }
 
     @Override
-    public OutputStream getOutputStream(String absolutePath) {
-        return null;
-    }
-
-    @Override
-    public String[] list(String path) {
-        return new String[0];
+    public String[] list(String path) throws IOException{
+        File target = new File(path);
+        if(!target.exists()){
+            log.warn("the path can not be found : " + path);
+        }
+        if(!target.isFile()){
+            log.warn("the path is not a file : " + path);
+        }
+        return target.list();
     }
 
     @Override
     public boolean deleteDirectory(String path) {
-        return false;
+        File target = new File(path);
+        if(!target.exists()){
+            log.warn("the path can not be found : " + path);
+            return false;
+        }
+        if(!target.isDirectory()){
+            log.warn("the file is not adirectory : " + path);
+            return false;
+        }
+        return target.delete();
     }
 
     @Override
-    public String lastModified(String path) {
-        return null;
+    public long lastModified(String path) throws IOException{
+        File target = new File(path);
+        if(target.exists()){
+            return target.lastModified();
+        }
+        return -1;
     }
 
     @Override
-    public long getSize(String path) {
-        return 0;
+    public long getSize(String path) throws IOException{
+        File target = new File(path);
+        return target.length();
     }
 
     @Override
-    public void touch(String path) {
+    public void touch(String path) throws IOException{
+        File target = new File(path);
+        boolean success = target.setLastModified(System.currentTimeMillis());
+        if(!success){
+            throw new IOException("unable to set lastModifiedTime !");
+        }
+    }
 
+    public static void main(String[] args) throws IOException{
+        CommonFileStorage file = new CommonFileStorage();
+        String path = "F://信息号.txt";
+        boolean b = file.exists(path);
+        String[] list =  file.list("F://");
+        System.out.println(list);
+        //InputStream is = file.getInputStream(path);
+        //OutputStream os = file.getOutputStream(path);
+        //boolean b = file.createNewFile(path);
+        //boolean c = file.deleteFile(path);
     }
 }
--- src/com/core/util/file/IFileStorage.java
+++ src/com/core/util/file/IFileStorage.java
@@ -1,21 +1,94 @@
 package com.core.util.file;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
  * 文件存储接口
+ * @author reborntodie
  */
 public interface IFileStorage {
-    long saveFile(String aboslutePath , InputStream input);
-    boolean createNewFile(String aboslutePath);
-    boolean deleteFile(String absolutePath);
-    boolean exists(String absolutePath);
-    InputStream getInputStream(String absolutePath);
-    OutputStream getOutputStream(String absolutePath);
-    String[] list(String path);
-    boolean deleteDirectory(String path);
-    String lastModified(String path);
-    long getSize(String path);
-    void touch(String path);
+    /**
+     *保存文件，保存后会close InputStream。若文件不存在，会创建新文件;若文件存在则先删除，再创建。
+     * @param aboslutePath
+     * @param input
+     * @return
+     * @throws IOException
+     */
+    long saveFile(String aboslutePath , InputStream input) throws IOException;
+
+    /**
+     * 创建空白新文件，创建成功返回true，否则返回false
+     * @param aboslutePath
+     * @return
+     */
+    boolean createNewFile(String aboslutePath) throws IOException;
+
+    /**
+     * 删除文件
+     * @param absolutePath
+     * @return
+     * @throws IOException
+     */
+    boolean deleteFile(String absolutePath) throws IOException;
+
+    /**
+     * 判断文件是否存在
+     * @param absolutePath
+     * @return
+     */
+    boolean exists(String absolutePath) throws IOException;
+
+    /**
+     * 获取文件输入流
+     * @param absolutePath
+     * @return
+     * @throws IOException
+     */
+    InputStream getInputStream(String absolutePath) throws IOException;
+
+    /**
+     * 获取文件输出流
+     * @param absolutePath
+     * @return
+     */
+    OutputStream getOutputStream(String absolutePath) throws IOException;
+
+    /**
+     * 列出目录下所有文件
+     * @param path
+     * @return
+     */
+    String[] list(String path) throws IOException;
+
+    /**
+     * 删除文件夹
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    boolean deleteDirectory(String path) throws IOException;
+
+    /**
+     *获取文件修改时间
+     * @param path
+     * @return
+     */
+    long lastModified(String path) throws IOException;
+
+    /**
+     * 获取文件大小
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    long getSize(String path) throws IOException;
+
+    /**
+     * 设置文件最后修改时间为当前时间
+     * @param path
+     * @throws IOException
+     */
+    void touch(String path) throws IOException;
 }
